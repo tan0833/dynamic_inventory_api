b9b6fb05684fa0cfdaf109ac52b23988442c0ced
John Doe <tanxueqiang@chinasoftinc.com>
2020-07-04 23:47:24 +0800

    aa

--- util/compare_dict.py
+++ util/compare_dict.py
@@ -1,67 +1,68 @@
-from __future__ import print_function
 import json
 
-def dict_generator(indict, pre=None):
-    pre = pre[:] if pre else []
-    if isinstance(indict, dict):
-        for key, value in indict.items():
-            if isinstance(value, dict):
-                if len(value) == 0:
-                    yield pre+[key, '{}']
-                else:
-                    for d in dict_generator(value, pre + [key]):
-                        yield d
-            elif isinstance(value, list):
-                if len(value) == 0:                   
-                    yield pre+[key, '[]']
-                else:
-                    for v in value:
-                        for d in dict_generator(v, pre + [key]):
-                            yield d
-            elif isinstance(value, tuple):
-                if len(value) == 0:
-                    yield pre+[key, '()']
-                else:
-                    for v in value:
-                        for d in dict_generator(v, pre + [key]):
-                            yield d
-            else:
-                yield pre + [key, value]
-    else:
-        yield indict
 
-if __name__ == "__main__":
-    sJOSN =  """{
-    "base_config":{
-        "enforce":{
-            "value":"0",
-            "inherit":"0",
-            "global":"0"
-        },
-        "modify":{
-            "value":"0",
-            "inherit":"0",
-            "global":"0"
-        }
-    },
-    "safe_control_list":{
-        "list":[
-            {
-                "gid":"0",
-                "gname":"全网计算机",
-                "isactive":"1",
-                "rule_id":"0",
-                "rule_name":"请选择规则",
-                "time_range":"所有时间",
-                "time_range_id":"1",
-                "policy_tpl":"33",
-                "policy_tpl_id":"17",
-                "isonline":"3",
-                "priority":"1"
-            }
-        ]
+
+class Compare_dict:
+
+
+    __temp_set = set()
+
+    def print_keyvalue_all(self,input_json):
+        '''
+        递归遍历json键用于比对两个json
+        :param input_json:
+        :return:
+        '''
+        if isinstance(input_json,dict):
+            for key in input_json.keys():
+                key_value = input_json.get(key)
+                if isinstance(key_value,dict):
+                    self.print_keyvalue_all(key_value)
+                elif isinstance(key_value,list):
+                    for json_array in key_value:
+                        self.print_keyvalue_all(json_array)
+                else:
+                    self.__temp_set.add(str(key)+ " = " +str(key_value))
+
+        elif isinstance(input_json,list):
+            for input_json_array in input_json:
+                self.print_keyvalue_all(input_json_array)
+
+        return self.__temp_set
+
+if __name__ == '__main__':
+    # json文件发送形式
+    SendRegisterVerificationCodejson_txt = """
+    {
+        "menus": [{
+            "key": "测试1",
+            "type": "Channel"
+        }, {
+            "key": "测试2"
+        }, {
+            "key": "测试3",
+            "type": "Channel"
+        }],
+        "test":"12",
+        "test01":{"a":1,"b":2}
     }
-}"""
-    sValue = json.loads(sJOSN)
-    for i in dict_generator(sValue):
-        print('.'.join(i[0:-1]), ':', i[-1])
+    """
+    date_json = json.loads(SendRegisterVerificationCodejson_txt)
+
+    # a = Compare_dict()
+    # b = a.print_keyvalue_all(date_json)
+    # print(b)
+
+    a = {"1":"2","3":"4","5":"6"}
+    b = '12345'
+
+    def repl1(aa,bb):
+        c = None
+        for i in aa.keys():
+            if i in bb:
+                value = aa[i]
+                c = bb.replace(i,value)
+        return c
+
+    c = repl1(a,b)
+    print(c)
